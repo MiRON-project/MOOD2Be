f097fa0650f99f50a04c2c85349fd87ca2def1b2
Davide Faconti <davide.faconti@eurecat.org>
2018-11-26 15:03:18 +0100

    initial skeleton

---
+++ IntralogisticExecutor/executor.cpp
@@ -0,0 +1,13 @@
+#include <behaviortree_cpp/blackboard/blackboard_local.h>
+#include <behaviortree_cpp/bt_factory.h>
+
+using namespace BT;
+
+int main()
+{
+
+    BehaviorTreeFactory factory;
+
+
+    return 0;
+}
