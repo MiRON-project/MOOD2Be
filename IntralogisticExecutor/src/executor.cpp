4b30e6f92ed1fb9126ae4b097500971f2a7c6d0e
Davide Faconti <davide.faconti@eurecat.org>
2018-11-28 14:36:07 +0100

    almost there...

--- IntralogisticExecutor/src/executor.cpp
+++ IntralogisticExecutor/src/executor.cpp
@@ -13,60 +13,21 @@
 
 using namespace BT;
 
+struct Arguments{
+    std::string skills_file;
+    std::string tree_file;
+    std::string IP;
+};
+
+Arguments ParseArguments(int argc, char** argv);
 
 int main(int argc, char** argv)
 {   
-    // Read more about args here: https://github.com/Taywee/args
+    Arguments arg = ParseArguments(argc, argv);
 
-    args::ArgumentParser parser("BehaviorTree.CPP Executor", "Load one or multiple plugins and the XML with the tree definition.");
-    args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
-
-    args::Group arguments(parser, "arguments", args::Group::Validators::DontCare, args::Options::Global);
-
-    args::ValueFlag<std::string> tree_path(arguments, "path", "The XML containing the BehaviorTree ",
-                                           {'t', "tree"}, args::Options::Required);
-
-    args::ValueFlag<std::string> skills_path(arguments, "path", "JSON file containing the list of SmartSoft skills",
-                                           {'s', "skills"}, args::Options::Required);
-
-    args::ValueFlag<std::string> server_ip(arguments, "ip", "IP of the server",
-                                           {"ip"}, "localhost" );
-
-    try
-    {
-        parser.ParseCLI(argc, argv);
-    }
-    catch (const args::Completion& e)
-    {
-        std::cout << e.what();
-        return 0;
-    }
-    catch (const args::Help&)
-    {
-        std::cout << parser;
-        return 0;
-    }
-    catch (const args::ParseError& e)
-    {
-        std::cerr << e.what() << std::endl;
-        std::cerr << parser;
-        return 1;
-    }
-    catch (const args::RequiredError& e)
-    {
-        std::cerr << "One of the mandatory arguments is missing:" << std::endl;
-        std::cerr << e.what() << std::endl;
-        std::cerr << parser;
-        return 1;
-    }
-
-    const std::string skills_file =  args::get(skills_path);
-    const std::string tree_file   =  args::get(tree_path);
-    const std::string IP =  args::get(server_ip);
-
-    std::cout << "\ttree file: " << tree_file << std::endl;
-    std::cout << "\tIP: " << IP << std::endl;
-    std::cout << "\tskills file: " << skills_file << std::endl;
+    std::cout << "\ttree file: "   << arg.tree_file << std::endl;
+    std::cout << "\tIP: "          << arg.IP << std::endl;
+    std::cout << "\tskills file: " << arg.skills_file << std::endl;
 
     //------------------------------------------------------
     // Populate the factory from Skills file and
@@ -74,20 +35,28 @@ int main(int argc, char** argv)
 
     zmq::context_t zmq_context(1);
 
-    auto definitions = ParseSkillFile( skills_file );
+    auto definitions = ParseSkillFile( arg.skills_file );
 
     BehaviorTreeFactory factory;
 
+    // register an action for each Skill
     for (const auto& def: definitions )
     {
-        auto creator = [def, &zmq_context, &IP](const std::string& name, const NodeParameters& params)
+        auto creator = [def, &zmq_context, &arg](const std::string& name, const NodeParameters& params)
         {
-            return std::unique_ptr<TreeNode>( new SkillAction(def, name, params, IP.c_str(), zmq_context) );
+            return std::unique_ptr<TreeNode>( new SkillAction(def, name, params, arg.IP.c_str(), zmq_context) );
         };
+        TreeNodeManifest manifest = { NodeType::ACTION, def.ID, def.params };
+        factory.registerBuilder( manifest, creator );
+    }
+
+    for (const auto& model : factory.manifests())
+    {
+        std::cout << model.registration_ID << std::endl;
     }
 
     auto blackboard = Blackboard::create<BlackboardLocal>();
-    auto tree = BT::buildTreeFromFile(factory, tree_file, blackboard);
+    auto tree = BT::buildTreeFromFile(factory, arg.tree_file, blackboard);
 
     // add loggers
     StdCoutLogger logger_cout(tree.root_node);
@@ -107,4 +76,62 @@ int main(int argc, char** argv)
         }
     }
     return 0;
+}
+
+///////////////////////////////////////////////////////////////
+
+Arguments ParseArguments(int argc, char** argv)
+{
+    // Read more about args here: https://github.com/Taywee/args
+
+    args::ArgumentParser parser("BehaviorTree.CPP Executor", "Load one or multiple plugins and the XML with the tree definition.");
+    args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+
+    args::Group arguments(parser, "arguments", args::Group::Validators::DontCare, args::Options::Global);
+
+    args::ValueFlag<std::string> tree_path(arguments, "path",
+                                           "The XML containing the BehaviorTree ", {'t', "tree"},
+                                           args::Options::Required);
+
+    args::ValueFlag<std::string> skills_path( arguments, "path",
+                                              "JSON file containing the list of SmartSoft skills", {'s', "skills"},
+                                              args::Options::Required);
+
+    args::ValueFlag<std::string> server_ip(arguments, "ip",
+                                           "IP of the server", {"ip"}, "localhost" );
+    try
+    {
+        parser.ParseCLI(argc, argv);
+    }
+    catch (const args::Completion& e)
+    {
+        std::cout << e.what();
+        exit(0);
+    }
+    catch (const args::Help&)
+    {
+        std::cout << parser;
+        exit(0);
+    }
+    catch (const args::ParseError& e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        exit(0);
+    }
+    catch (const args::RequiredError& e)
+    {
+        std::cerr << "One of the mandatory arguments is missing:" << std::endl;
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        exit(0);
+    }
+
+    Arguments output;
+
+    output.skills_file =  args::get(skills_path);
+    output.tree_file   =  args::get(tree_path);
+    output.IP =  args::get(server_ip);
+
+    return output;
 }
--- IntralogisticExecutor/src/skill_parser.cpp
+++ IntralogisticExecutor/src/skill_parser.cpp
@@ -1,4 +1,7 @@
 #include <fstream>
+#include <unordered_set>
+#include <algorithm>
+#include <string>
 #include "Intralogistic/skill_interface.hpp"
 #include "Intralogistic/json.hpp"
 
@@ -16,6 +19,10 @@ std::vector<SkillDefinition> ParseSkillFile(const std::string &filename)
     nlohmann::json doc;
     input_file >> doc;
 
+    const std::unordered_set<std::string> supported_types =
+    {
+        "String", "Int", "Double", "Integer"
+    };
 
     for (const auto& item: doc)
     {
@@ -23,18 +30,20 @@ std::vector<SkillDefinition> ParseSkillFile(const std::string &filename)
         SkillDefinition definition;
         definition.ID = skill["name"];
         definition.skill_FQN = skill["skill-definition-fqn"];
-        //std::cout << definition.ID << std::endl;
 
         const auto& inAttributes = skill["in-attribute"];
 
         for (auto it = inAttributes.begin(); it != inAttributes.end(); it++)
         {
             std::string key   = it.key();
-            std::string value = it.value();
+            std::string type = it.value();
 
-            //std::cout << key << " = " << value << std::endl;
+            if( supported_types.count(type) == 0)
+            {
+                throw std::runtime_error("Error in [in-attribute]: We don't support this type: " + type);
+            }
 
-            definition.params.insert( { key, value} );
+            definition.params.insert( { key, type} );
         }
 
         const auto& results = skill["results"];
@@ -77,8 +86,87 @@ std::string GenerateRequest(const SkillDefinition& definition,
     auto& skill = json["skill"];
     skill["name"] = definition.ID;
     skill["skill-definition-fqn"] = definition.skill_FQN;
-    skill["in-attribute"] = current_params;
     skill["out-attribute"] = nlohmann::json({});
+    skill["in-attribute"] = current_params;
 
     return json.dump(indent);
+}
+
+std::string SkillAction::generateRequest()
+{
+    nlohmann::json json;
+    json["msg-type"] = "push-skill";
+    json["id"] = _current_uid;
+
+    auto& skill = json["skill"];
+    skill["name"] = _definition.ID;
+    skill["skill-definition-fqn"] = _definition.skill_FQN;
+    skill["out-attribute"] = nlohmann::json({});
+    auto& in_attribute = skill["in-attribute"] = nlohmann::json({});
+
+    // read node parameters
+    for(const auto& param: initializationParameters() )
+    {
+        const auto& key  = param.first;
+        const auto& type = _definition.params.at(key);
+
+        if( type == "String")
+        {
+            auto val = getParam<std::string>(key);
+            if( !val ){
+                throw std::runtime_error( "Invalid parameter at key: " + key );
+            }
+            in_attribute.push_back( {key, val.value() } );
+        }
+        else if( type == "Double")
+        {
+            auto val = getParam<double>(key);
+            if( !val ){
+                throw std::runtime_error( "Invalid parameter at key: " + key );
+            }
+            in_attribute.push_back( {key, val.value() } );
+        }
+        else if( type == "Integer" || type == "Int" )
+        {
+            auto val = getParam<int>(key);
+            if( !val ){
+                throw std::runtime_error( "Invalid parameter at key: " + key );
+            }
+            in_attribute.push_back( {key, val.value() } );
+        }
+    }
+
+    return json.dump(1);
+}
+
+BT::NodeStatus SkillAction::convertResultToStatus(const std::string &result_string)
+{
+    nlohmann::json json = nlohmann::json::parse(result_string);
+    std::string msg_type  = json["msg-type"];
+    std::string result    = json["result"]["result"];
+    auto json_res_value = json["result"]["result-value"];
+
+    //TODO
+    // get and check the identifier
+
+    if( json_res_value.is_string() )
+    {
+        const std::string bb_result_key = _definition.ID + "::last_result";
+        std::string res_value = json_res_value;
+        blackboard()->set( bb_result_key, res_value );
+    }
+
+    std::transform(result.begin(), result.end(), result.begin(), ::toupper);
+    if( result == "SUCCESS")
+    {
+        return BT::NodeStatus::SUCCESS;
+    }
+    else if( result == "ERROR")
+    {
+        return BT::NodeStatus::FAILURE;
+    }
+    else{
+        std::cout << "Error parsing this reply:\n" << result << std::endl;
+        throw std::runtime_error("Unrecognized [result]" );
+    }
 }
